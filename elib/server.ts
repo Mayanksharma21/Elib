6b9dfd7ca2bd4abd11b62d59b02fb31db09c946e
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2024-07-17 01:24:34 +0530

    Configure Environment Variable

--- elib/server.ts
+++ elib/server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { envConfig } from "./src/config/config";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = envConfig.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
