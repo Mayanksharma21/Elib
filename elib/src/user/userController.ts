c1efa2a801d5ad9d650544b90f9d82139f4f4963
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2024-07-18 00:47:44 +0530

    refactor controller

--- elib/src/user/userController.ts
+++ elib/src/user/userController.ts
@@ -21,27 +21,37 @@ const handleUserRegister = async (
 
   // Process / Business Logic
 
-  const isExistedUser = await userModel.findOne({ email });
+  try {
+    const isExistedUser = await userModel.findOne({ email });
 
-  if (isExistedUser) {
-    const error = createHttpError(400, "User already exists");
-    return next(error);
+    if (isExistedUser) {
+      const error = createHttpError(400, "User already exists");
+      return next(error);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = sign({ sub: newUser._id }, envConfig.jwt_secret as string, {
+      expiresIn: "7d",
+    });
+
+    // Response
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+
+    const customError = createHttpError(
+      500,
+      "Server Error while registering!!"
+    );
+    return next(customError);
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
-  const token = sign({ sub: newUser._id }, envConfig.jwt_secret as string, {
-    expiresIn: "7d",
-  });
-
-  // Response
-  return res.status(201).json({ token });
 };
 
 export { handleUserRegister };
